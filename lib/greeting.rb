93aaca01a3f4e897274e7ae466b2ce1bc70c0ede
Melinda Burke <melccb@gmail.com>
2018-09-29 05:01:15 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,7 @@
 # code the #greeting method here!
 
 def greeting(name)
-  puts "Hello #{name}. It's nice to meet you. I am HAL 9000 computer. Would you like to hear me sing?"
+  puts "Hello #{name}. It's nice to meet you.
 end
 
 def goodbye (name)
